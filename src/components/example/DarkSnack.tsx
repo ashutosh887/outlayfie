29092c397213777a1d74f42ce9218ff376f0cc69
Ashutosh Jha <ashutoshj887@gmail.com>
2023-12-27 16:47:49 +0530

    feat: update examples

---
+++ src/components/example/DarkSnack.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleProp,
+  ViewStyle,
+  useColorScheme,
+} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import Main from '../common/Main';
+import {appName} from '../../utils/common';
+
+export function DarkSnack(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle: StyleProp<ViewStyle> = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <Main title={appName} version={appName} />
+    </SafeAreaView>
+  );
+}
