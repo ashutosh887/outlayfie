765ef0e602ea4edb3ff31b2aebb2925d8b7f3f0a
Ashutosh Jha <ashutoshj887@gmail.com>
2023-12-29 15:56:12 +0530

    feat: set data configs

---
+++ src/config/data.ts
@@ -0,0 +1,29 @@
+import {TRIP} from './types';
+
+export const sampleTrips: TRIP[] = [
+  {
+    id: '1',
+    country: 'India',
+    city: 'Bengaluru',
+  },
+  {
+    id: '2',
+    country: 'India',
+    city: 'Bhopal',
+  },
+  {
+    id: '3',
+    country: 'India',
+    city: 'Jalandhar',
+  },
+  {
+    id: '4',
+    country: 'India',
+    city: 'Gokarna',
+  },
+  {
+    id: '5',
+    country: 'India',
+    city: 'Gokarna',
+  },
+];
