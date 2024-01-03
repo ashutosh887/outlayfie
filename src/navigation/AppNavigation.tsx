a29146600ed4e5126b4f04dc645071f720ac86c5
Ashutosh Jha <ashutoshj887@gmail.com>
2024-01-03 18:18:52 +0530

    fix: app navigation

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 import HomeScreen from '../screens/HomeScreen';
-import LoginScreen from '../screens/LoginScreen';
 import AddExpenseScreen from '../screens/AddExpenseScreen';
 import AddTripScreen from '../screens/AddTripScreen';
 import AllTripsScreen from '../screens/AllTripsScreen';
 import {AppStackNavigationParams} from '../config/types';
 import TripExpensesScreen from '../screens/TripExpenses';
+import WelcomeScreen from '../screens/WelcomeScreen';
+import SignInScreen from '../screens/SignInScreen';
+import SignUpScreen from '../screens/SignUpScreen';
 
 const Stack = createNativeStackNavigator<AppStackNavigationParams>();
 
@@ -15,10 +17,11 @@ const AppNavigation = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Home"
+        initialRouteName="Welcome"
         screenOptions={{
           headerShown: false,
         }}>
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen
           name="Home"
           component={HomeScreen}
@@ -27,10 +30,17 @@ const AppNavigation = () => {
           }}
         />
         <Stack.Screen
-          name="Login"
-          component={LoginScreen}
+          name="SignIn"
+          component={SignInScreen}
           options={{
-            animation: 'fade',
+            presentation: 'modal',
+          }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUpScreen}
+          options={{
+            presentation: 'modal',
           }}
         />
         <Stack.Screen
